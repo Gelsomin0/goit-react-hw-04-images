5dc5302370f61d68ed9ed42e69d10feb5312f07c
Ivan <120274876+Gelsomin0@users.noreply.github.com>
2023-08-04 21:41:33 +0300

    some changes

--- src/tools/getSearchData.js
+++ src/tools/getSearchData.js
@@ -2,5 +2,5 @@ const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '37084690-566054f69b0903988f9ff1e75';
 
 export const getSearchData = (searchText, page) => {
-    return fetch(`${BASE_URL}?q=${searchText}&page=1&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12&page=${page}`);
+    return fetch(`${BASE_URL}?q=${searchText.toLowerCase()}&page=1&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12&page=${page}`);
 }
